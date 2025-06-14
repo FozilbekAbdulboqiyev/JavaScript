5075865315fb5e2bb1f1fb0dea7e9838c24ca384
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-14 21:32:53 +0500

    JS all examples

--- 26-Dars/index.js
+++ 26-Dars/index.js
@@ -1,1 +1,32 @@
-console.log("hi");
+// let arr = [7, 1, 6, 3, 9, 20]
+
+// function getDate() {
+//     let a =this
+//     a.sort((a , b)=>{
+//         return a - b
+//     })
+//     console.log(this); 
+//    a.length 
+//   console.log(a - this);
+// }
+// getDate.call(arr)
+//========================================================================================================
+let obj = {
+    name:"Eshmat",
+    Age:20,
+    status:"Talaba",
+    
+}
+
+let total = 0
+
+function twise(){
+    let a= this
+    total+=obj?.Age
+    console.log(this);
+    a.person.forEach(person => {
+    calculate(person);
+      });
+}
+twise.call(obj)
+console.log(total);
