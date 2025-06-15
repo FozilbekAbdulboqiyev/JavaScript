aab62cebcfb272385c01107b20fe9de45ad938d3
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-15 21:54:50 +0500

    JS  all example

--- 26-Dars/index.js
+++ 26-Dars/index.js
@@ -11,21 +11,47 @@
 // }
 // getDate.call(arr)
 //========================================================================================================
-let obj = {
-    people: [
-        { name: "Ali", age: 25 },
-        { name: "Vali", age: 30 },
-        { name: "Eshmat", age: 20 }
-    ]
-};
+// let obj = {
+//     people: [
+//         { name: "Ali", age: 25 },
+//         { name: "Vali", age: 30 },
+//         { name: "Eshmat", age: 20 }
+//     ]
+// };
 
-let total = 0;
+// let total = 0;
 
-function getDate() {
-    this.people.forEach(person => {
-        total += person.age;
-    });
+// function getDate() {
+//     this.people.forEach(person => {
+//         total += person.age;
+//     });
+// }
+
+// getDate.call(obj);
+// console.log(total)
+//===========================================================================================================
+let user = { name: "Gulmat" };
+
+function greet() {
+    console.log("Salom, " + this.name);
 }
 
-getDate.call(obj);
-console.log(total)
+greet.call(user);
+//============================================================================================================
+ let product = {price: 100};
+ function pay(){
+    return this.price*1.16
+
+ };
+ let finalprise=pay.call(product)
+ console.log(finalprise)
+
+let person={
+    firstName:"Eshmat",
+    lastName:"Gulmatov",
+}
+function full(){
+    console.log(this.firstName+this.lastName);
+}
+full.call(person)
+//Functioncall
