04797a4e99f38ebe9c2fe26f4be5f2b972a8c81b
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-15 22:04:17 +0500

    JS  all example

--- 26-Dars/index.js
+++ 26-Dars/index.js
@@ -30,28 +30,57 @@
 // getDate.call(obj);
 // console.log(total)
 //===========================================================================================================
-let user = { name: "Gulmat" };
+// let user = { name: "Gulmat" };
 
-function greet() {
-    console.log("Salom, " + this.name);
-}
+// function greet() {
+//     console.log("Salom, " + this.name);
+// }
 
-greet.call(user);
-//============================================================================================================
- let product = {price: 100};
- function pay(){
-    return this.price*1.16
+// greet.call(user);
+// //============================================================================================================
+//  let product = {price: 100};
+//  function pay(){
+//     return this.price*1.16
 
- };
- let finalprise=pay.call(product)
- console.log(finalprise)
+//  };
+//  let finalprise=pay.call(product)
+//  console.log(finalprise)
 
-let person={
-    firstName:"Eshmat",
-    lastName:"Gulmatov",
-}
-function full(){
-    console.log(this.firstName+this.lastName);
-}
-full.call(person)
+// let person={
+//     firstName:"Eshmat",
+//     lastName:"Gulmatov",
+// }
+// function full(){
+//     console.log(this.firstName+this.lastName);
+// }
+// full.call(person)
 //Functioncall
+// function yigim(){
+//     return this.prices.reduce((sum,price)=>sum+price,0)
+// }
+// let store ={
+//     prices:[100,200,300]
+// };
+// let total =yigim.apply(store)
+// console.log(total);
+// //============================================================================================
+// function kvadrat() {
+//     console.log(this.num * 2);
+// }
+
+// let obj = { num: 7 };
+// kvadrat.apply(obj);
+// //============================================================================================
+// function decorateSymbol(symbol) {
+//     console.log(symbol + this.text + symbol);
+// }
+
+// let obj = { text: "JavaScript" };
+// decorateSymbol.apply(obj, ["  3oy  "]); 
+//====================================================================================
+function showAge() {
+    console.log("Yosh: " + this.age);
+}
+
+let user = { age: 19 };
+showAge.apply(user); 
