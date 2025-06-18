5c9a022d612c71c24521eabec264a129d58d1e4d
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-18 09:40:23 +0500

    JS  all example

--- Takrorlash2/index.js
+++ Takrorlash2/index.js
@@ -31,6 +31,31 @@
 // let musbat=sonlar.findIndex(son => son < 0);
 // console.log(musbat);
 //Findlastindex bu shartga moskelgan elemntni oxirgi indexsini chiqaradi
-let sonlar=[-1,0,3,9]
-let musbat=sonlar.findLastIndex(son => son < 2);
-console.log(musbat);
+// let sonlar=[-1,0,3,9]
+// let musbat=sonlar.findLastIndex(son => son < 2);
+// console.log(musbat);
+//sort bu arraydigi elenmentlarni saralab tartiblaydi
+// let mevalar=["olma","ananas","banan"]
+// mevalar.sort()
+// console.log(mevalar);
+//every bu arry ichidagi hamma elementni tekshiradi agar hammasi shartga too'g'ri kelsa true qaytaradi
+// let sonlar=[3,9]
+// let musbat=sonlar.every(son => son > 0);
+// console.log(musbat); 
+//some bu shartdagi 1ta elemnt  to'g'ri keladin bo'lsa ham true qaytaradi
+// let sonlar=[-3,-1,0,9]
+// let musbat=sonlar.some(son => son >0);
+// console.log(musbat);
+//flat bu ichi ma ich ketgan arraylarni bitta arrayga tekislaydi
+//  let arr =[1,2,3,[5,4]];
+//  let newArr=arr.flat();
+//  console.log(newArr);
+//fill bu arr dagi barcha qiymatlarni boshqa bir qiymatlarbilan berilgan qiymatda to'ldirish;
+//  let arr =[1,2,3,4,5];
+//  arr.fill(6)
+//  console.log(arr);
+//arr ichidagi qismini olib shu arrni boshqa joyiga nusxalaydi
+// let arr =[1,2,3,4,5,6]
+// arr.copyWithin(0,3,5)
+// console.log(arr);
+
