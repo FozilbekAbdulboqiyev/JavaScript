757c39f25a6be2b77122e1ce3677a1801865d694
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-18 09:00:34 +0500

    JS  all example

--- Takrorlash2/index.js
+++ Takrorlash2/index.js
@@ -1,11 +1,18 @@
-let sonlar =[1,2,3,4,5];
-sonlar.forEach(function(son){
-    console.log(son);
+//foreach--- bu array ichidagi harbir element ustida amal bajaradi
+// let sonlar =[1,2,3,4,5];
+// sonlar.forEach(function(son){
+//     console.log(son);
     
-})
+// })
 
-let mevalar=["olma","olma","banan"]
-mevalar.forEach(function(meva,index){
-    console.log(index + ':'+meva);
+// let mevalar=["olma","olma","banan"]
+// mevalar.forEach(function(meva,index){
+//     console.log(index + ':'+meva);
     
+// })
+//map ham foreachga oxshaydi
+let sonlar=[1,2,3,4,5];
+let newNumber =sonlar.map(function(son){
+    return son*2
 })
+console.log(newNumber);
