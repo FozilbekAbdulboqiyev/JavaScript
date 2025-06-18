52540bf42d9e697235fae23ff2c87d7452d3ffec
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-18 08:50:53 +0500

    JS  all example

---
+++ Takrorlash2/index.js
@@ -0,0 +1,11 @@
+let sonlar =[1,2,3,4,5];
+sonlar.forEach(function(son){
+    console.log(son);
+    
+})
+
+let mevalar=["olma","olma","banan"]
+mevalar.forEach(function(meva,index){
+    console.log(index + ':'+meva);
+    
+})
