cd518ee4773bd6e32c606612febfc1c9b9009e26
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-20 08:41:47 +0500

    JS  all example

--- 29-Dars/index.js
+++ 29-Dars/index.js
@@ -1,6 +1,15 @@
-let arr=["Ali",["Eshmat","Gulmat",["Toshmat","Bahridin","Muxriddin"]]]
+// let arr=["Ali",["Eshmat","Gulmat",["Toshmat","Bahridin","Muxriddin"]]]
 
-Array.prototype.salom = function() {
-    console.log(this.flat(Infinity));
-}
-arr.salom(); 
+// Array.prototype.salom = function() {
+//     console.log(this.flat(Infinity));
+// }
+// arr.salom(); 
+
+let arr=[1,2,3,4,5]
+ Array.prototype.hi=function(val){
+ let newArr=this.filter((valeu)=> valeu !== val);
+ console.log(newArr);
+ }
+ arr.hi(3)
+ console.log(arr);
+ 
