6c33d07581cf6c7e39981b6e86de028549fb7524
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-21 22:16:46 +0500

    JS  all example

--- Takrorlash2/index.js
+++ Takrorlash2/index.js
@@ -133,3 +133,14 @@ console.log(NewArr);
 
 
 // NewArr = 1,2,3,4,5,6 Kelyapti
+// 1-----------------------------------------------------------------------------------------------------------------
+function fib(n){
+    if (n===0)return 0
+    else if(n===1){
+        return 1
+    }
+    else{
+        return fib (n -1)+fib(n-2)
+    }
+}
+console.log(fib(5));
