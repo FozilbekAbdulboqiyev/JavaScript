60e4b7f66aef206c715708257d2b99e58b76ccf0
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-27 09:09:20 +0500

    JS  all example

--- 30-Dars/index.js
+++ 30-Dars/index.js
@@ -1,13 +1,23 @@
-class Parent {
-    constructor(props){
-     console.log(props);
+// class Parent {
+//     constructor(props){
+//      console.log(props);
      
-    }
-    getName(props){
-        console.log(this.name, "va", props );
+//     }
+//     getName(props){
+//         console.log(this.name, "va", props );
         
+//     }
+// }
+// let user=new Parent("Eshmat");
+// // console.log(user.name);
+// user.getName("Toshmat")
+//class dagi glabal o'zgaruvchi bu klass dan tashqarida elon qilingan o'zgaruvchi .Uni class ichida o'qish va o'zgartirish mumkin
+let count =0;
+class Parent{
+    getData(){
+        count += 10;
+        console.log(count)
     }
 }
-let user=new Parent("Eshmat");
-// console.log(user.name);
-user.getName("Toshmat")
+let obj = new Parent()
+obj.getData()
