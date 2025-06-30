0ca955c4dd7433509ca5fff11314abbd8cda1a7e
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-06-30 16:32:44 +0500

    JS  all example

---
+++ 31-Dars/index.js
@@ -0,0 +1,12 @@
+class MyClass {
+    static myStaticVar = 'Bu static o‘zgaruvchi';
+  
+    static myStaticMethod() {
+      console.log('Static method chaqirildi');
+    }
+  
+    instanceMethod() {
+      console.log('Bu oddiy instance method');
+    }
+  }
+  
