cd3f98b763e324d6b077af8082e43c5f6e0ed956
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-07-03 19:48:39 +0500

    JS  all example

---
+++ 33-Dars/index.js
@@ -0,0 +1,16 @@
+class Clock {
+    showTime() {
+      setInterval(() => {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        console.log(`${hours}:${minutes}:${seconds}`);
+      }, 1000);
+    }
+  }
+  
+  const clock = new Clock();
+  clock.showTime();
+  
+  
