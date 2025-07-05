555edb8f312ed9e2a093f59c3b84f7cc34b96f81
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-07-05 22:08:40 +0500

    JS  all example

--- 33-Dars/index.js
+++ 33-Dars/index.js
@@ -1,14 +1,14 @@
-//1=====================================================
-// class Clock{
-//    count = 0;
-//    getData(){
-//     console.log("04:00:0",this.count++)
-//   }
-// }
-// let obj = new Clock()
-// obj.getData()
-// obj.getData()
-// obj.getData()
-// obj.getData()
-// obj.getData()
+// 1=====================================================
+class Clock{
+   count = 0;
+   getData(){
+    console.log("04:00:0",this.count++)
+  }
+}
+let obj = new Clock()
+obj.getData()
+obj.getData()
+obj.getData()
+obj.getData()
+obj.getData()
 
--- Takrorlash/index.js
+++ Takrorlash/index.js
@@ -7,3 +7,18 @@ else{
     console.log('juft')
     console.log( 5 + 3);
 }   
+let nestedArr = [1, 2, [3, 4, [5, 6, [7, [8, [9, [10, [11, [12, [13]]]]]]]]]];
+
+let newArr = [];
+function recFunction(arr) {
+  if (arr.length) {
+    for (let value of arr) {
+      if (Array.isArray(value)) return recFunction(value);
+      else {
+        newArr.push(value);
+      }
+    }
+  }
+}
+recFunction(nestedArr);
+console.log(newArr);
