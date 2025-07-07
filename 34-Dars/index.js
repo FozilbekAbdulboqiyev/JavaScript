879ca84668935f9720b4fa2761be1043c7b53d94
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-07-07 16:02:22 +0500

    JS  all example

--- 34-Dars/index.js
+++ 34-Dars/index.js
@@ -6,11 +6,21 @@
 //      console.log(error.message)
 //      console.log(error.stack)
 // }
-function getFullName(prop){
-  prop();
-}
-let callFunc =() =>{
-  console.log("Eshmat","Toshmatov");
+// function getFullName(prop){
+//   prop();
+// }
+// let callFunc =() =>{
+//   console.log("Eshmat","Toshmatov");
   
-}
-getFullName(callFunc)
+// }
+// getFullName(callFunc)
+setTimeout(()=>{
+  console.log("1-Bosqich");
+  setTimeout(() =>{
+    console.log("2-Bosqich");
+    setTimeout(()=>{
+      console.log("3-Bosqich");
+      
+    },3000)
+  },2000)
+},1000)
