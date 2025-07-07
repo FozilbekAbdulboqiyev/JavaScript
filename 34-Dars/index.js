01d5478b7124f88968b7db5939fb379d0ee5e7cd
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-07-07 15:34:34 +0500

    JS  all example

--- 34-Dars/index.js
+++ 34-Dars/index.js
@@ -1,8 +1,16 @@
-try{
-  let obj =undefined
-  obj.someMethod();
-} catch (error){
-     console.log(error.name)
-     console.log(error.message)
-     console.log(error.stack)
+// try{
+//   let obj =undefined
+//   obj.someMethod();
+// } catch (error){
+//      console.log(error.name)
+//      console.log(error.message)
+//      console.log(error.stack)
+// }
+function getFullName(prop){
+  prop();
 }
+let callFunc =() =>{
+  console.log("Eshmat","Toshmatov");
+  
+}
+getFullName(callFunc)
