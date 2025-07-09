0803d3946cd41edc7c2ef8c01a2e59fc077244e0
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-07-09 11:02:06 +0500

    JS  all example

--- 35-Dars/index.js
+++ 35-Dars/index.js
@@ -11,38 +11,47 @@
 // console.log(obj.next().value);
 // console.log(obj.next().value);
 
-
 function* getData(val){
-   while(1){
-    yield val++
-   }
-}
-let obj = getData(1)
+    while(1){
+      yield val++
+    }
+  }
 
-let arr =[
-    { id:obj.next().value, name:"Eshmat",},
-    { id:obj.next().value, name:"Toshmat",},
-    { id:obj.next().value, name:"Gulmat",},
-    { id:obj.next().value, name:"Ummat",},
-    { id:obj.next().value, name:"Urmat",},
-    
-]
-let list = document.getElementById("list")
-function setUser(){
-    list.innerHTML=null
-    arr.map((obj)=>{
-        let h1=document.createElement("h1");
-        h1.innerHTML = `${obj?.id}-${obj.name} <button onclick="del(${obj?.id})">Delete</button>`;
-        list.append(h1)
-    })
-}
-setUser();
-function addUser(){
-    arr.push({id:obj.next().value,name:"Gulbashakar",})
-    setUser(); 
-    
-}
-let del =(id)=>{
-   arr = arr.filter((obj)=> obj?.id !== id)
-   setUser(); 
-}
+  let obj = getData(1);
+
+  let arr = [
+    { id: obj.next().value, name: "Eshmat" },
+    { id: obj.next().value, name: "Toshmat" },
+    { id: obj.next().value, name: "Gulmat" },
+    { id: obj.next().value, name: "Ummat" },
+    { id: obj.next().value, name: "Urmat" },
+  ];
+
+  let list = document.getElementById("list");
+
+  function setUser(){
+    list.innerHTML = null;
+    arr.map((obj) => {
+      let h1 = document.createElement("h1");
+      h1.innerHTML = `${obj.id} - ${obj.name} <button onclick="del(${obj.id})">Delete</button>`;
+      list.append(h1);
+    });
+  }
+
+  setUser();
+
+  function addUser(){
+    let input = document.getElementById("nameInput");
+    let name = input.value
+
+    if(name){
+      arr.push({ id: obj.next().value, name: name });
+      setUser();
+      input.value = ""; // inputni tozalaydi
+    } 
+  }
+
+  let del = (id) => {
+    arr = arr.filter((obj) => obj.id !== id);
+    setUser();
+  };
