4e3ba7aae73be796af9a2fab2e93dc116d09603e
Fozilbek <fozilbekabdulboqiyev52@gmail.com>
2025-07-25 12:37:00 +0500

    JS  all example

---
+++ 39-Dars/index.js
@@ -0,0 +1,32 @@
+// let body = document.body;
+// let btn1=document.querySelector(".me_1")
+// let btn2=document.querySelector(".me_2")
+// btn1.onclick = ()=>{
+//     body.scrollIntoView(false);
+// }
+// btn2.onclick = ()=>{
+//     body.scrollIntoView();
+// }
+// window.scroll(0,1500)
+// btn.onclick =()=>{
+//     window.scrollBy({
+//         top:1000,
+//         behavior:"smooth",
+//     })
+// }
+// let ball = document.getElementById("ball");
+// let field = document.getElementById("field");
+// let button = document.getElementById("centerBtn");
+
+// button.addEventListener("click", () => {
+//   let ballWidth = ball.offsetWidth;
+//   let ballHeight = ball.offsetHeight;
+//   let centerX = (field.clientWidth - ballWidth) / 2;
+//   let centerY = (field.clientHeight - ballHeight) / 2;
+//   ball.style.left = centerX + "px";
+//   ball.style.top = centerY + "px";
+// });
+// window.scroll(0, 500)
+// window.scrollTo({ top: 1000, left: 0, behavior: "smooth" });
+// window.scrollBy({ top: 300, behavior: "smooth" });
+document.getElementById("mySection").scrollIntoView({ behavior: "smooth" });
